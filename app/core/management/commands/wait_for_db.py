cbd51ba58523204731005e5ea7318eb931359a76
Turyahabwa Mark <profturyahabwa@gmail.com>
2023-01-18 10:51:16 +0300

    feat: add mgt command and test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""
+Django management command for waiting for the postgres db to finish setting up.
+"""
+import time
+
+from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+    
+    def handle(self, *args, **options):
+        """Entrypoint for commands"""
+        self.stdout.write('Waiting for database connection ... ')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default']) # check for problems method
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write(self.style.WARNING('Database unavailable, Waiting 1 second ...'))
+                time.sleep(1)
+                
+        self.stdout.write(self.style.SUCCESS('Database available'))
+
