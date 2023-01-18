99d941107dcad296d155487757e529bf8214cc6c
Turyahabwa Mark <profturyahabwa@gmail.com>
2023-01-18 16:12:11 +0300

    feat: Add normalise email

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Creates, saves and returns a new user"""
-        user = self.model(email=email, **extra_field)
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,15 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_normalise_email_successful(self):
+        """Test whether poorly input emails are normalized"""
+        sample_inputs = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+        for given_email, expected in sample_inputs:
+            user = get_user_model().objects.create_user(given_email, "Sample123")
+            self.assertEqual(user.email, expected)
